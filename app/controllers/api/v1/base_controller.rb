79a5b4c61c11c9e1095f98b20ee7bbbe2d6a213e
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-05-21 19:42:04 +0200

    add controllers for api

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::BaseController < ActionController::Base
+  rescue_from StandardError, with: :internal_server_error
+
+  private
+
+  def internal_server_error(exception)
+    if Rails.env.development?
+      response = { type: exception.class.to_s, error: exception.message }
+    else
+      response = { error: "Internal Server Error" }
+    end
+    render json: response, status: :internal_server_error
+  end
+end
