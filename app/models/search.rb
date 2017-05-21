050af8965d18b611f352080ac7e421d19b068517
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-05-21 19:18:10 +0200

    Create search class

---
+++ app/models/search.rb
@@ -0,0 +1,59 @@
+class Search
+  attr_reader :name, :org_number, :revenue, :registration_year, :phone, :address
+
+  def initialize(search_term)
+    @search_term = search_term
+    search_for_company if search_term.present?
+  end
+
+  def as_json
+    {
+      name: @name,
+      revenue: @revenue,
+      registration_year: @registration_year,
+      phone: @phone,
+      address: @address
+    }
+  end
+
+  private
+
+  def search_for_company
+    # Search on allabolag.se with the given search term
+    doc = nokogiri_doc("http://www.allabolag.se/what/#{@search_term}")
+    # First search result
+    search_result = doc.css('.search-results__item__details').first
+    # If there was a match
+    if search_result
+      # Get the organisation number
+      @org = search_result.css('dd').first.text
+      get_company_info
+    end
+  end
+
+  def get_company_info
+    # Get detailed information about the company
+    company_doc = nokogiri_doc("http://www.allabolag.se/#{@org.gsub "-", ""}")
+    # Company name
+    @name = get_attribute(company_doc, '.p-name')
+    # Last year's revenue
+    @revenue = company_doc.css('*:contains("Omsättning") + *')
+    @revenue = @revenue.empty? ? "not available" : @revenue.first.text.strip + " 000 SEK"
+    # Registration year
+    @registration_year = get_attribute(company_doc, 'dt:contains("Registreringsår") + dd')
+    # Phone number
+    @phone = get_attribute(company_doc, '.p-tel')
+    # Address
+    @address = company_doc.css('.p-postal-code').first.parent.text.gsub("\n", "")
+  end
+
+  def get_attribute(company_doc, css_selector)
+    attribute = company_doc.css(css_selector)
+    attribute.empty? ? "not available" : attribute.first.text.strip
+  end
+
+  def nokogiri_doc(url)
+    require 'open-uri'
+    Nokogiri::HTML(open(url))
+  end
+end
